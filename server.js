019b6e6a6e8715f874be648e278a92c90051d748
nard.calma@gmail.com <nard.calma@gmail.com>
2022-08-20 07:58:15 -0700

    View dir created, index route created and tested

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/plan", (req,res) => {
+    res.render("index.ejs")
+})
+
 app.listen(PORT,()=>{
     console.log(`Server is running at PORT: ${PORT}`)
 })
