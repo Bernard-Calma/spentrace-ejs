e8bf88ea0232a704e94f8ab5636f004c00b09dbf
nard.calma@gmail.com <nard.calma@gmail.com>
2022-08-23 20:04:05 -0700

    Made running total shows different every entry

--- controllers/plans.js
+++ controllers/plans.js
@@ -10,13 +10,29 @@ router.route("/")
 //Index
 .get((req,res) => {
     Plan.find({},(err,plans) => {
+        let total = 0;
         let runningTotal = 0;
+        let totalIncome = 0;
+        let totalExpense = 0;
+        let target = 0;
         plans.forEach(element => {
+            target = 0;
             // console.log(element.date.toString().slice(4,15))
             element.date = element.date.toString().slice(4,15)
-            runningTotal += element.amount
+            if (element.expense === true) {
+                totalExpense += element.amount
+            } else if (element.expense === false) {
+                totalIncome += element.amount
+            } 
+            total += element.amount
+            if (runningTotal < 0) {
+                target = Math.abs(total)
+            } else {
+                target = 0;
+            }
+            
         });
-        res.render("index.ejs", {plans,runningTotal})
+        res.render("index.ejs", {plans,total,totalExpense,target})
     })
 })
 .post((req,res) => {
