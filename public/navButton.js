13b0c57545310f64a8fbd42b9aa9094b8785bef8
nard.calma@gmail.com <nard.calma@gmail.com>
2022-08-24 19:11:00 -0700

    added add and back button for index page

--- public/navButton.js
+++ public/navButton.js
@@ -1,7 +1,47 @@
 const button = document.querySelector("#button-outer")
-const content = document.querySelector(".index")
+const nav = document.querySelector("nav")
+let buttonBack = document.querySelector("#button-back")
+let buttonClicked = false;
 button.addEventListener("click", ()=>{
-    const newLine = document.createElement("li")
-    newLine.innerText = "test"
-    content.appendChild(newLine)
+    // console.log(nav)
+    // const newLine = document.createElement("li")
+    // newLine.innerText = "test"
+    // content.appendChild(newLine)
+    if (!buttonClicked) {
+        nav.innerHTML = `
+            <div style="width: 60%;">
+            
+            </div>
+            <div id="button-outer">
+                <div id="button-inner">
+                    <a class="button-clicked" href="/plan/new">+</a>
+                </div>
+            </div>
+            <div id="button-back">
+                <div id="button-inner">
+                <a class="button-clicked" href="/plan">⏎</a>
+                </div>
+            </div>
+            <div style="width: 20%;">
+
+            </div>
+        `
+
+        buttonClicked = true
+    } else {
+        nav.innerHTML = `
+            <div style="width: 60%;">
+            
+            </div>
+            <div id="button-outer">
+                <div id="button-inner">
+                    =
+                </div>
+            </div>
+            <div style="width: 20%;">
+
+            </div>
+        `
+        buttonClicked = false
+    }    
 })
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({extended:true}))
 app.use("/plan",planController)
 
 app.get('/', (req,res) => {
-    res.redirect("/plan")
+    res.render("login.ejs")
 })
 
 app.listen(PORT,()=>{
